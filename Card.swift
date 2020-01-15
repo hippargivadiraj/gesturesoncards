a833727c6e06271982c3bc3d07bae33bee9b5148
Vadiraj Hippargi <Vadi@leadconsultant.com>
2020-01-15 07:25:42 -0800

    Added Scale Effect

--- Card.swift
+++ Card.swift
@@ -10,17 +10,30 @@ import SwiftUI
 
 struct Card: View {
     
+    @State private var scale: CGFloat = 1
+    
     let tapped: Bool
     var body: some View {
         VStack{
-            
+            Image("Cat")
+                .resizable()
+            .scaleEffect(self.scale )
+                .frame(width: 300, height: 300)
+                .gesture(MagnificationGesture()
+                    .onChanged{ value in
+                        self.scale = value.magnitude
+                    }
+                .onEnded{ value in
+                    self.scale = 1
+                    }
+            )
     Text("Card")
         .font(.largeTitle)
         .foregroundColor(Color.white)
 
         }
-//        .frame(minWidth: 0 , maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
-            .frame(width: 400, height: 300, alignment: .center)
+        .frame(minWidth: 0 , maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+//            .frame(width: 400, height: 300, alignment: .center)
             .background(self.tapped ? Color.orange : Color.purple)
         .cornerRadius(30)
         
